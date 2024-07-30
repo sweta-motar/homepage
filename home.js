3dfeb1601f790906db1bc8ac755ec9d04bcacd09
sweta-motar <166835605+sweta-motar@users.noreply.github.com>
2024-07-30 12:05:27 +0530

    Update home.js

--- home.js
+++ home.js
@@ -47,3 +47,34 @@ function toggleNavbar() {
     var navbar = document.getElementById("navbar");
     navbar.classList.toggle("active");
 }*/
+//script for about us and contact
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+});
+
+// Smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+//about
+document.getElementById('about-link').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    //document.getElementById('homepage').scrollIntoView({ behavior: 'smooth' });
+    window.location = "about.html";
+  });
+//contact
+document.getElementById('contact-link').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    //document.getElementById('homepage').scrollIntoView({ behavior: 'smooth' });
+    window.location = "about.html";
+  });
